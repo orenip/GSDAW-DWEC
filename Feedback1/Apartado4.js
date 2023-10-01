2bc5f570d341a633c9d37f90818008be8786fa8a
orenip <josanlopi@hotmail.com>
2023-10-01 13:37:31 +0200

    first commit

---
+++ Feedback1/Apartado4.js
@@ -0,0 +1,1 @@
+window.alert('Hola soy Jose Antonio López Piñero desde un archivo externo')
---
+++ LMF/23-24/tutoria02.js
@@ -0,0 +1,4 @@
+let boton = document.getElementsByTagName("button")[0]
+        boton.addEventListener("click", function(){
+            alert('hola')
+        })
